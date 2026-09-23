f532e80863aa6fc6d7e9e68d708432273bacc433
juanamarinansky <juanamarinansky1@gmail.com>
2026-09-23 10:35:07 -0300

    UI, menú inicial y botones

---
+++ BioStick3D_proyecto/Assets/scripts/ui - Menú inicial/MenuManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void Jugar()
+    {
+        SceneManager.LoadScene("proyectoMain");
+    }
+
+    public void AbrirTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+}
